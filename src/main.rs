a8889076578e672a46c41ae091cf8085a6e048a6
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-08-29 13:45:16 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+
+fn main()-> Result<()> {
+    let listener = TcpListener::bind("localhost:3000")?;
+    for connection in listener.incoming(){
+        let stream: TcpStream = connection?;
+        handle_connection(stream)?;
+    }
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut request = Vec::new();
+    let mut reader = BufReader::new(&mut stream);
+    reader
+        .read_until(b'\n', &mut request)?;
+
+    let request = String::from_utf8(request)?;// convert bytes to human readable format
+    print!("HTTP request line: {}",request);
+
+    let response = concat!(
+        "HTTP/1.1 200 OK\r\n",
+        "Content-Length : 121 \r\n",
+        "Content-Type: text/html \r\n\r\n",
+        "<!DOCTYPE html> <html> <body> <h1>Enter a search term below:</h1> </body> </html>"
+    );
+
+    stream.write(response.as_bytes())?;
+    stream.flush()?;
+    Ok(())
+
+}
