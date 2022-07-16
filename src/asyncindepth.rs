71a3a23c18ffa3a2b5dd991eae2f79fba22fa827
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-07-16 10:39:51 -0400

    added macro

--- src/asyncindepth.rs
+++ src/asyncindepth.rs
@@ -15,3 +15,17 @@ pub fn asyncmain() -> Result<()> {
         revents: 0,
     }];
 }
+
+fn wait(fds: &mut [libc::pollfd]) -> Result<usize> {}
+
+macro_rules! syscall {
+    ($fn: ident $args:tt) => {
+        let res = unsafe {libc::$fn $args};
+        if res == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(res)
+        }
+
+    };
+}
