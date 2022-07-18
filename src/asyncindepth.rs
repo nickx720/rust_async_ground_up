2e888f96f59fdd087db896f2c10e1dc543007eac
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-07-18 06:59:55 -0400

    adding closure to file descriptor

--- src/asyncindepth.rs
+++ src/asyncindepth.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::io::Result;
 use std::net::TcpListener;
 use std::os::unix::prelude::AsRawFd;
 
-/// https://youtu.be/_3LpJ6I-tzc?t=427
+/// https://youtu.be/_3LpJ6I-tzc?t=569
 
 pub fn asyncmain() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7000")?;
@@ -14,14 +15,43 @@ pub fn asyncmain() -> Result<()> {
         events: libc::POLLIN,
         revents: 0,
     }];
+    let mut handlers: HashMap<i32, Box<dyn Fn()>> = HashMap::new();
+    handlers.insert(
+        fd,
+        Box::new(move || {
+            let (client, addr) = listener.accept().unwrap();
+            handle_connection(client, addr);
+        }),
+    );
+    loop {
+        let num_events = wait(&mut polls_fds)?;
+        if num_events > 0 {
+            for poll_fd in &poll_fds {
+                if poll_fd.revents & libc::POLLIN != 0 {
+                    println!(
+                        "poll_fd {} received revents {}",
+                        poll_fd.fd, poll_fd.revents
+                    );
+                    let (client, addr) = listener.accept().unwrap();
+                    handle_connection(client, addr);
+                }
+            }
+        }
+    }
 }
 
 fn wait(fds: &mut [libc::pollfd]) -> Result<usize> {
-    syscall!(poll(
-        fds.as_mut_ptr() as *mut libc::pollfd,
-        fds.len() as libc::nfds_t,
-        -1
-    ))
+    loop {
+        match syscall!(poll(
+            fds.as_mut_ptr() as *mut libc::pollfd,
+            fds.len() as libc::nfds_t,
+            -1
+        )) {
+            Ok(n) => break Ok(n as usize),
+            Err(e) if e.raw_os_err() == Some(libc::EAGAIN) => continue,
+            Err(e) => return Err(e),
+        }
+    }
 }
 
 macro_rules! syscall {
