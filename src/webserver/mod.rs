0234bf57935bad156d8d3382507dd0f110e2bf58
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-08-12 08:34:46 -0400

    Adding child process to start new connections

--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -1,3 +1,4 @@
+use std::io::Error;
 use std::io::Result;
 use std::net::TcpListener;
 mod node;
@@ -6,6 +7,17 @@ mod router;
 mod routes;
 
 use router::Router;
+
+fn check_err(num: i32) -> Result<i32> {
+    if num < 0 {
+        return Err(Error::last_os_error());
+    }
+    Ok(num)
+}
+
+fn fork() -> Result<u32> {
+    check_err(unsafe { libc::fork() }).map(|pid| pid as u32)
+}
 
 pub fn webservermain() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7000")?;
@@ -17,7 +29,12 @@ pub fn webservermain() -> Result<()> {
     routes::configure(&mut router);
 
     for client in listener.incoming() {
-        router.route_client(client?);
+        let chiild_pid = fork()?;
+        if chiild_pid == 0 {
+            router.route_client(client?);
+        } else {
+            println!("[{pid} forking process, new {chiild_pid}]");
+        }
     }
     Ok(())
 }
