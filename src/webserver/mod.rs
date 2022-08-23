80ba9573bba236548133291fbaa219fdbb587667
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-08-23 08:12:58 -0400

    can't use ? , client must be unwrapped

--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -46,7 +46,7 @@ pub fn webservermain() -> Result<()> {
                         "[{pid}] {:?} client connected at",
                         std::thread::current().id()
                     );
-                    // router.route_client(client?);
+                    router.route_client(client.unwrap());
                 });
                 handles.push(handle);
             }
