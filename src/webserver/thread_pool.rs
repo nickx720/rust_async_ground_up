ccb7a4cbceafc522aa3f1d3b0ebf8c8b0e6a96eb
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-08-30 08:33:42 -0400

    changes

--- src/webserver/thread_pool.rs
+++ src/webserver/thread_pool.rs
@@ -1,3 +1,13 @@
 pub struct Threadpool {
     workers: Vec<Worker>,
 }
+
+impl Drop for Threadpool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            if let Some(handle) = worker.thread.take() {
+                handle.join().unwrap();
+            }
+        }
+    }
+}
