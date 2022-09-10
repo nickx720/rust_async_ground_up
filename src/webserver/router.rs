8701a9eb6af08337466d6d528bceb91ef7ca0feb
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-09-10 07:59:30 -0400

    adding job type

--- src/webserver/router.rs
+++ src/webserver/router.rs
@@ -6,7 +6,7 @@ use std::net::TcpStream;
 
 pub type HandlerFn = fn(TcpStream) -> Result<()>;
 
-/// https://youtu.be/fdxhcDne2Ww?t=2316
+/// https://youtu.be/fdxhcDne2Ww?t=2538
 
 #[derive(PartialEq, Eq, Hash)]
 pub enum Method {
--- src/webserver/worker.rs
+++ src/webserver/worker.rs
@@ -8,9 +8,16 @@ pub struct Worker {
     thread: Option<thread::JoinHandle<()>>,
 }
 
+pub type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<_>>>) -> Self {
-        let handle = thread::spawn(move || loop {});
+        let handle = thread::spawn(move || loop {
+            let task = {
+                let rx = receiver.lock().unwrap();
+                rx.recv().unwrap()
+            }
+        });
 
         Worker {
             id,
