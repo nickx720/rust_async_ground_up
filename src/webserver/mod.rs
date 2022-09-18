50f3b3f85dab8ed7b48bb153a31960ac9d47015b
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-09-18 06:46:58 -0400

    wip nonblocking

--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -1,6 +1,7 @@
 use std::io::Error;
 use std::io::Result;
 use std::net::TcpListener;
+use std::os::unix::prelude::AsRawFd;
 use std::sync::Arc;
 use std::thread;
 mod node;
@@ -87,4 +88,20 @@ pub fn webservermain() -> Result<()> {
         println!("[{pid}] <<< {p} exited")
     }
     Ok(())
+}
+
+pub fn webserverasyncmain() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7000")?;
+    listener.set_nonblocking(true)?;
+
+    let fd = listener.as_raw_fd();
+    let mut poll_fds = vec![libc::pollfd {
+        fd,
+        events: libc::POLLIN,
+        revents: 0,
+    }];
+    loop {
+        let num_events = todo!();
+    }
+    todo!()
 }
